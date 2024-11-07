bf651262ad4c5d0b1bfa4993ba719b0111fc5fd7
Abbass Tabikh <abbasstabikh87774@gmail.com>
2024-11-07 11:39:57 +0200

    angular contacts app

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,32 +1,45 @@
 import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Gender} from '../gender';
 import {Contact} from '../models/contact';
+import {JsonPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    JsonPipe,
+    NgIf
   ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent implements OnChanges {
   protected readonly Gender = Gender;
-  public gender: Gender | undefined;
-  public name: string | undefined;
-  public phoneNumber: string | undefined;
-
   @Input() selectedContact : Contact | undefined;
   @Output() addContactEvent = new EventEmitter<Contact>();
   @Output() updateContactEvent = new EventEmitter<Contact>();
 
+  // Define the FormGroup
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = fb.group({
+      name: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      gender: ['', Validators.required]
+    });
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['selectedContact'] && changes['selectedContact'].currentValue) {
-      this.name = this.selectedContact?.name;
-      this.gender = this.selectedContact?.gender;
-      this.phoneNumber = this.selectedContact?.phone;
+      this.contactForm.patchValue({
+        name: this.selectedContact?.name,
+        phoneNumber: this.selectedContact?.phone,
+        gender: this.selectedContact?.gender
+      });
     }
 
     else
@@ -34,33 +47,33 @@ export class ContactFormComponent implements OnChanges {
   }
 
   SaveContact() {
+
+    let { name, phoneNumber, gender } = this.contactForm.value;
+
     // case update
     if(this.selectedContact) {
-      if (this.phoneNumber !== undefined)
-        this.selectedContact.phone = this.phoneNumber;
-
-      if (this.name !== undefined)
-        this.selectedContact.name = this.name;
-
-      if (this.gender)
-        this.selectedContact.gender = this.gender;
+      this.selectedContact.name = name; //from contact form
+      this.selectedContact.phone = phoneNumber; //from contact form
+      this.selectedContact.gender = gender; //from contact form
 
       this.updateContactEvent.emit(this.selectedContact);
       this.selectedContact = undefined;
-      this.ResetFields();
-      return;
     }
 
     //case add
-    let contact = new Contact(this.name ? this.name : "", this.phoneNumber ? this.phoneNumber : "Not Set", this.gender ? this.gender : Gender.Male);
-    console.log(contact?.gender, Gender.Female, contact.gender === Gender.Female);
-    this.addContactEvent.emit(contact);
+    else{
+      let contact = new Contact(name, phoneNumber, gender);
+      this.addContactEvent.emit(contact);
+    }
+
     this.ResetFields();
   }
 
   private ResetFields(): void {
-    this.phoneNumber = undefined;
-    this.gender = undefined;
-    this.name = undefined;
+    this.contactForm.patchValue({
+      name: '',
+      phoneNumber: '',
+      gender: undefined
+    });
   }
 }
